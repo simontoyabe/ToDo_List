ae09155f7f8d6de95dd7b2aa09ebf4a940991627
Simón Montoya Bedoya <simonmontoyabedoya@gmail.com>
2021-07-26 19:17:09 -0500

    [FIX] button links on create account page

--- scripts/crear_cuenta.js
+++ scripts/crear_cuenta.js
@@ -10,7 +10,7 @@ window.onload = () => {
     const email = formLogin.email;
 
     botonLogin.addEventListener("click", () => {
-        location.href = "./login.html";
+        location.href = "./index.html";
     });
 
     formLogin.addEventListener('submit', (event) => {
